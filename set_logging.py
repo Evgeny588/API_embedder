3128c298548b1664d416e8dd0faf23aba561b679
Evgeny <evgeborisov@gmail.com>
2026-04-19 16:03:15 +0300

    Some debug

--- set_logging.py
+++ set_logging.py
@@ -1,7 +1,6 @@
 import sys
 from loguru import logger
 
-
 def setup_logging(name: str):
     """
     Настройка логгера loguru.
@@ -13,16 +12,15 @@ def setup_logging(name: str):
         sys.stdout,
         format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
         level="INFO",
-        colorize=True,
+        colorize=True
     )
 
     logger.add(
         "logs/app_logs.log",
-        rotation="10 MB",
+        rotation="10 MB", 
         retention="7 days",
         compression="zip",
-        level="DEBUG",
+        level="DEBUG"
     )
 
-    # Привязываем имя модуля к контексту логгера
     return logger.bind(name=name)
